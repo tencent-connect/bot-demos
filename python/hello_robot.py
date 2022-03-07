5fc26f03b346e63b15b2c97dbbdd2bc895d044aa
veehou <veehou@tencent.com>
2022-03-07 16:50:25 +0800

    feat: 修改ark和embed消息的构造方式

--- python/hello_robot.py
+++ python/hello_robot.py
@@ -46,64 +46,11 @@ async def _message_handler(event, message: qqbot.Message):
 
 
 async def _create_ark_obj_list(weather_dict) -> List[MessageArkObj]:
-    obj_list = []
-
-    obj = MessageArkObj()
-    obj_kv_list = []
-    obj_kv = MessageArkObjKv()
-    obj_kv.key = "desc"
-    obj_kv.value = weather_dict['result']['citynm'] + " " + weather_dict['result']['weather']
-    obj_kv_list.append(obj_kv)
-    obj.obj_kv = obj_kv_list
-    obj_list.append(obj)
-
-    obj = MessageArkObj()
-    obj_kv_list = []
-    obj_kv = MessageArkObjKv()
-    obj_kv.key = "desc"
-    obj_kv.value = "当日温度区间：" + weather_dict['result']['temperature']
-    obj_kv_list.append(obj_kv)
-    obj.obj_kv = obj_kv_list
-    obj_list.append(obj)
-
-    obj = MessageArkObj()
-    obj_kv_list = []
-    obj_kv = MessageArkObjKv()
-    obj_kv.key = "desc"
-    obj_kv.value = "当前温度：" + weather_dict['result']['temperature_curr']
-    obj_kv_list.append(obj_kv)
-    obj.obj_kv = obj_kv_list
-    obj_list.append(obj)
-
-    obj = MessageArkObj()
-    obj_kv_list = []
-    obj_kv = MessageArkObjKv()
-    obj_kv.key = "desc"
-    obj_kv.value = "当前湿度：" + weather_dict['result']['humidity']
-    obj_kv_list.append(obj_kv)
-    obj.obj_kv = obj_kv_list
-    obj_list.append(obj)
-
+    obj_list = [MessageArkObj(obj_kv=[MessageArkObjKv(key="desc", value=weather_dict['result']['citynm'] + " " + weather_dict['result']['weather'])]),
+                MessageArkObj(obj_kv=[MessageArkObjKv(key="desc", value="当日温度区间：" + weather_dict['result']['temperature'])]),
+                MessageArkObj(obj_kv=[MessageArkObjKv(key="desc", value="当前温度：" + weather_dict['result']['temperature_curr'])]),
+                MessageArkObj(obj_kv=[MessageArkObjKv(key="desc", value="当前湿度：" + weather_dict['result']['humidity'])])]
     return obj_list
-
-
-async def _create_ark_kv_list(weather_dict) -> List[MessageArkKv]:
-    kv_list = []
-    kv = MessageArkKv()
-    kv.key = "#DESC#"
-    kv.value = "描述"
-    kv_list.append(kv)
-
-    kv = MessageArkKv()
-    kv.key = "#PROMPT#"
-    kv.value = "提示消息"
-    kv_list.append(kv)
-
-    kv = MessageArkKv()
-    kv.key = "#LIST#"
-    kv.obj = await _create_ark_obj_list(weather_dict)
-    kv_list.append(kv)
-    return kv_list
 
 
 async def send_weather_ark_message(weather_dict, channel_id, message_id):
@@ -118,35 +65,13 @@ async def send_weather_ark_message(weather_dict, channel_id, message_id):
     ark = MessageArk()
     # 模版ID=23
     ark.template_id = 23
-    ark.kv = await _create_ark_kv_list(weather_dict)
+    ark.kv = [MessageArkKv(key="#DESC#", value="描述"),
+              MessageArkKv(key="#PROMPT#", value="提示消息"),
+              MessageArkKv(key="#LIST#", obj=await _create_ark_obj_list(weather_dict))]
     # 通过api发送回复消息
     send = qqbot.MessageSendRequest(content="", ark=ark, msg_id=message_id)
     msg_api = qqbot.AsyncMessageAPI(t_token, False)
     await msg_api.post_message(channel_id, send)
-
-
-async def _create_embed_fields(weather_dict) -> List[MessageEmbedField]:
-    fields = []
-    field = MessageEmbedField()
-    field.name = "当日温度区间：" + weather_dict['result']['temperature']
-    fields.append(field)
-
-    field = MessageEmbedField()
-    field.name = "当前温度：" + weather_dict['result']['temperature_curr']
-    fields.append(field)
-
-    field = MessageEmbedField()
-    field.name = "最高温度：" + weather_dict['result']['temp_high']
-    fields.append(field)
-
-    field = MessageEmbedField()
-    field.name = "最低温度：" + weather_dict['result']['temp_low']
-    fields.append(field)
-
-    field = MessageEmbedField()
-    field.name = "当前湿度：" + weather_dict['result']['humidity']
-    fields.append(field)
-    return fields
 
 
 async def send_weather_embed_direct_message(weather_dict, guild_id, user_id):
@@ -166,7 +91,11 @@ async def send_weather_embed_direct_message(weather_dict, guild_id, user_id):
     thumbnail.url = weather_dict['result']['weather_icon']
     embed.thumbnail = thumbnail
     # 构造内嵌消息fields
-    embed.fields = await _create_embed_fields(weather_dict)
+    embed.fields = [MessageEmbedField(name="当日温度区间：" + weather_dict['result']['temperature']),
+                    MessageEmbedField(name="当前温度：" + weather_dict['result']['temperature_curr']),
+                    MessageEmbedField(name="最高温度：" + weather_dict['result']['temp_high']),
+                    MessageEmbedField(name="最低温度：" + weather_dict['result']['temp_low']),
+                    MessageEmbedField(name="当前湿度：" + weather_dict['result']['humidity'])]
 
     # 通过api发送回复消息
     send = qqbot.MessageSendRequest(embed=embed, content="")
